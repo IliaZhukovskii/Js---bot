e7aeed33c7897c19a4b3cd154fc646c1c240d048
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-31 14:49:10 +0300

    Готовый 6 урок

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+
+alert("Угадай число от 1 до 100");
+
+
+function newNum(){
+  let enterNum = prompt("Введите число");
+
+  function bot(num){
+
+    if (enterNum > num ){
+        alert("Загаданное число меньше");
+        newNum();
+    } else if (isNaN(enterNum) == true ){
+        alert("Вы ввели не число");
+        newNum();
+    } else if (enterNum === null ){
+        alert("Игра окончена");
+    } else if (enterNum < num ){
+        alert("Загаданное число больше");
+        newNum();
+    } else if (enterNum == num ){
+        alert("Поздравляю, Вы угадали!!!");
+    } 
+  }
+
+  bot(50);
+}
+
+newNum();
+  
+
