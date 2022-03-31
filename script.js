9b41afdab2ea97c4d61820b5ca73e9634c2b6c2b
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-31 16:02:57 +0300

    6 урок готовое  усложнённое задание

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 'use strict';
 
 
-alert("Угадай число от 1 до 100");
+alert("Угадай число от 1 до 100. У тебя 10 попыток");
 
-
-function newNum(){
-  let enterNum = prompt("Введите число");
+function newNum(count){
 
   function bot(num){
 
+    let enterNum = prompt("Введите число");
+
+    count--;
+
     if (enterNum > num ){
-        alert("Загаданное число меньше");
-        newNum();
+        alert("Загаданное число меньше, осталось попыток: " + count);
+        bot(num);
+    } else if(count == 0){
+        let newAttempt = confirm("Попытки закончились, хотите сыграть еще?");
+
+        if (newAttempt == true){
+            count = 10;
+            bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);          
+        } else {
+          alert("Игра окончена");
+        }
+
     } else if (isNaN(enterNum) == true ){
-        alert("Вы ввели не число");
-        newNum();
+        alert("Вы ввели не число, осталось попыток: " + count);
+        bot(num);
+
     } else if (enterNum === null ){
         alert("Игра окончена");
+
     } else if (enterNum < num ){
-        alert("Загаданное число больше");
-        newNum();
+        alert("Загаданное число больше, осталось попыток: " + count);
+        bot(num);
+
     } else if (enterNum == num ){
-        alert("Поздравляю, Вы угадали!!!");
+
+      let quest = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть ещё?");
+      
+        if (quest == true){
+          alert("Теперь предстоит угадать другое число");
+          bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);
+        } else {
+          alert("Игра окончена");
+        }
     } 
   }
 
   bot(50);
 }
 
-newNum();
+newNum(10);
   
 
