8dd0a4b6145126648f51603e92be3653587c772e
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-01 10:48:40 +0300

    исправил баги

--- script.js
+++ script.js
@@ -1,56 +1,50 @@
 'use strict';
 
 
-alert("Угадай число от 1 до 100. У тебя 10 попыток");
 
-function newNum(count){
 
-  function bot(num){
+function newNum(count) {
+    
+    alert("Угадай число от 1 до 100. У тебя 10 попыток");
 
-    let enterNum = prompt("Введите число");
-
-    count--;
-
-    if (enterNum > num ){
-        alert("Загаданное число меньше, осталось попыток: " + count);
-        bot(num);
-    } else if(count == 0){
-        let newAttempt = confirm("Попытки закончились, хотите сыграть еще?");
-
-        if (newAttempt == true){
-            count = 10;
-            bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);          
+    function bot(num) {
+        let enterNum = prompt("Введите число");
+        count--;
+        if (isNaN(enterNum) == true) {
+            count++;
+            alert("Вы ввели не число, осталось попыток: " + count);
+            bot(num);
         } else {
-          alert("Игра окончена");
+            if (count == 0) {
+                let newAttempt = confirm("Попытки закончились, хотите сыграть еще?");
+                if (newAttempt == true) {
+                    count = 10;
+                    bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);
+                } else {
+                    alert("Игра окончена");
+                }
+            } else if (enterNum > num) {
+                alert("Загаданное число меньше, осталось попыток: " + count);
+                bot(num);
+            } else if (enterNum === null) {
+                alert("Игра окончена");
+            } else if (enterNum < num) {
+                alert("Загаданное число больше, осталось попыток: " + count);
+                bot(num);
+            } else if (enterNum == num) {
+                let quest = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть ещё?");
+                if (quest == true) {
+                    alert("Теперь предстоит угадать другое число");
+                    count = 10;
+                    bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);
+                } else {
+                    alert("Игра окончена");
+                }
+            }
         }
+    }
 
-    } else if (isNaN(enterNum) == true ){
-        alert("Вы ввели не число, осталось попыток: " + count);
-        bot(num);
-
-    } else if (enterNum === null ){
-        alert("Игра окончена");
-
-    } else if (enterNum < num ){
-        alert("Загаданное число больше, осталось попыток: " + count);
-        bot(num);
-
-    } else if (enterNum == num ){
-
-      let quest = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть ещё?");
-      
-        if (quest == true){
-          alert("Теперь предстоит угадать другое число");
-          bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);
-        } else {
-          alert("Игра окончена");
-        }
-    } 
-  }
-
-  bot(50);
+    bot(Math.floor(Math.random() * (100 - 0 + 1)) + 0);
 }
 
 newNum(10);
-  
-
