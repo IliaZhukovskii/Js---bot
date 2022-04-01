77c9bba2a19e81009b8c620d2146d3d26b771e21
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-01 10:21:53 +0300

    исправил баги

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 'use strict';
 
 
-alert("Угадай число от 1 до 100");
+
 
 
 function newNum(){
-  let enterNum = prompt("Введите число");
-
+  let enterNum = prompt("Угадай число от 1 до 100. Введи число");
   function bot(num){
-
-    if (enterNum > num ){
-        alert("Загаданное число меньше");
-        newNum();
-    } else if (isNaN(enterNum) == true ){
+    
+    if (isNaN(enterNum) == true ){
         alert("Вы ввели не число");
         newNum();
-    } else if (enterNum === null ){
-        alert("Игра окончена");
-    } else if (enterNum < num ){
-        alert("Загаданное число больше");
-        newNum();
-    } else if (enterNum == num ){
-        alert("Поздравляю, Вы угадали!!!");
+    } else {
+        if (enterNum > num ){
+            alert("Загаданное число меньше");
+            newNum();
+    }   else if (enterNum === null ){
+            alert("Игра окончена");
+    }   else if (enterNum < num ){
+            alert("Загаданное число больше");
+            newNum();
+    }   else if (enterNum == num ){
+            alert("Поздравляю, Вы угадали!!!");
     } 
+    }
   }
 
   bot(50);
